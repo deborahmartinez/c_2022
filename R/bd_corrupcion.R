604e877a069449f6590e465687842a1028aa7591
Déborah <deborahandrea.m@hotmail.com>
2022-02-07 02:16:55 -0600

    bases de control

--- R/bd_corrupcion.R
+++ R/bd_corrupcion.R
@@ -6,11 +6,14 @@ library(readxl)
 # 2015 --------------------------------------------------------------
 
 #Percepción sobre la frecuencia de corrupción
-aux1<- read_csv("Variables/Corrupción/Utilizadas/frecuencia_corrupcion_15.csv")
+aux1<- read_csv("Variables/Corrupción/Modificadas manualmente/frecuencia_corrupcion_15.csv")
 
 #corrupción en diversas instituciones y sectores
-aux2 <- read_csv("Variables/Corrupción/Utilizadas/corrupción en diversas instituciones y sectores.csv",skip = 3)
+aux2 <- read_csv("Variables/Corrupción/Modificadas manualmente/corrupción en diversas instituciones y sectores.csv",skip = 3)
 aux2 <-aux2 %>% filter(!is.na(entidad), !is.na(MuyFrecuente_tot&Muy_frecuente))
+
+aux2 <- aux2 %>%  filter(grupo %in% c("Diputados y Senadores", "Ejército y Marina",
+"Gobierno Federal", "Gobiernos Estatales","Gobiernos Municipales", "Ministerio Público", "Policías" ))
 
 aux2 <- aux2 %>%  gather("respuesta", "valor", MuyFrecuente_tot:Nunca) %>% 
   mutate(grupo= gsub(pattern = " ", replacement = "_", x = grupo),
@@ -22,12 +25,12 @@ aux2 <- aux2 %>%  gather("respuesta", "valor", MuyFrecuente_tot:Nunca) %>%
   spread(key = grupo, value = valor)
 
 #Usuarios  que experimentaron algún acto de corrupción
-aux3 <- read_csv("Variables/Corrupción/Utilizadas/Usuarios  que experimentaron algún acto de corrupción.csv",skip=3)
+aux3 <- read_csv("Variables/Corrupción/Modificadas manualmente/Usuarios  que experimentaron algún acto de corrupción.csv",skip=3)
 aux3 <-aux3 %>%  select(entidad, contacto_tot, contacto)
 
 
 #Total trámites en los que existió experiencia de corrupción
-aux4 <- read_csv("Variables/Corrupción/Utilizadas/otal trámites en los que existió experiencia de corrupción.csv", skip = 1)
+aux4 <- read_csv("Variables/Corrupción/Modificadas manualmente/otal trámites en los que existió experiencia de corrupción.csv", skip = 1)
 aux4 <- aux4
 
 bd15 <- aux1 %>%  left_join(aux2, by = "entidad") %>%  left_join(aux3, by = "entidad") %>% 
@@ -36,11 +39,16 @@ bd15 <- aux1 %>%  left_join(aux2, by = "entidad") %>%  left_join(aux3, by = "ent
 
 # 2017 --------------------------------------------------------------------
 #Percepción sobre la frecuencia de corrupción
-aux1<- read_csv("Variables/Corrupción/Utilizadas/frecuencia_corrupcion_17.csv")
+aux1<- read_csv("Variables/Corrupción/Modificadas manualmente/frecuencia_corrupcion_17.csv")
 
 #corrupción en diversas instituciones y sectores
-aux2 <- read_csv("Variables/Corrupción/Utilizadas/corrupción en diversas instituciones y sectores 17.csv",skip = 2)
+aux2 <- read_csv("Variables/Corrupción/Modificadas manualmente/corrupción en diversas instituciones y sectores 17.csv",skip = 2)
 aux2 <-aux2 %>% filter(!is.na(entidad), !is.na(MuyFrecuente_tot&Muy_frecuente))
+
+aux2 <- aux2 %>%  filter(grupo %in% c("Diputados y Senadores", "Ejército y Marina",
+                                      "Gobierno Federal", "Gobiernos Estatales",
+                                      "Gobiernos Municipales", 
+                                      "Ministerio Público", "Policías" ))
 
 aux2 <- aux2 %>%  gather("respuesta", "valor", MuyFrecuente_tot:Nunca) %>% 
   mutate(grupo= gsub(pattern = " ", replacement = "_", x = grupo),
@@ -52,12 +60,12 @@ aux2 <- aux2 %>%  gather("respuesta", "valor", MuyFrecuente_tot:Nunca) %>%
   spread(key = grupo, value = valor)
 
 #Usuarios  que experimentaron algún acto de corrupción
-aux3 <- read_csv("Variables/Corrupción/Utilizadas/Usuarios  que experimentaron algún acto de corrupción 17.csv",skip=3)
+aux3 <- read_csv("Variables/Corrupción/Modificadas manualmente/Usuarios  que experimentaron algún acto de corrupción 17.csv",skip=3)
 aux3 <-aux3 %>%  select(entidad, contacto_tot, contacto)
 
 
 #Total trámites en los que existió experiencia de corrupción
-aux4 <- read_csv("Variables/Corrupción/Utilizadas/otal trámites en los que existió experiencia de corrupción 17.csv", skip =2)
+aux4 <- read_csv("Variables/Corrupción/Modificadas manualmente/otal trámites en los que existió experiencia de corrupción 17.csv", skip =2)
 
 
 bd17 <- aux1 %>%  left_join(aux2, by = "entidad") %>%  left_join(aux3, by = "entidad") %>% 
@@ -67,12 +75,19 @@ bd17 <- aux1 %>%  left_join(aux2, by = "entidad") %>%  left_join(aux3, by = "ent
 # 2019 --------------------------------------------------------------------
 
 #Percepción sobre la frecuencia de corrupción
-aux1<- read_csv("Variables/Corrupción/Utilizadas/frecuencia_corrupcion_19.csv")
+aux1<- read_csv("Variables/Corrupción/Modificadas manualmente/frecuencia_corrupcion_19.csv", skip = 3)
 
 #corrupción en diversas instituciones y sectores
-aux2 <- read_csv("Variables/Corrupción/Utilizadas/corrupción en diversas instituciones y sectores 19.csv",skip = 3)
+aux2 <- read_csv("Variables/Corrupción/Modificadas manualmente/corrupción en diversas instituciones y sectores 19.csv",skip = 3)
 aux2 <-aux2 %>% filter(!is.na(entidad), !is.na(MuyFrecuente_tot&Muy_frecuente))
 
+aux2<- aux2 %>%  
+  filter(grupo %in% c("Cámaras de Diputados y Senadores", "Ejército y Marina",
+                                      "Gobierno Federal", "Gobiernos Estatales",
+                      "Gobiernos Municipales",  "Ministerio Público",
+                      "Policías" )) %>% 
+  mutate(grupo =case_when(grupo =="Cámaras de Diputados y Senadores"~"Diputados y Senadores",
+                          T~grupo))
 aux2 <- aux2 %>%  gather("respuesta", "valor", MuyFrecuente_tot:Nunca) %>% 
   mutate(grupo= gsub(pattern = " ", replacement = "_", x = grupo),
          grupo = gsub(pattern = "í", replacement = "i", x = grupo),
@@ -83,12 +98,12 @@ aux2 <- aux2 %>%  gather("respuesta", "valor", MuyFrecuente_tot:Nunca) %>%
   spread(key = grupo, value = valor)
 
 #Usuarios  que experimentaron algún acto de corrupción
-aux3 <- read_csv("Variables/Corrupción/Utilizadas/Usuarios  que experimentaron algún acto de corrupción 19.csv",skip=3)
+aux3 <- read_csv("Variables/Corrupción/Modificadas manualmente/Usuarios  que experimentaron algún acto de corrupción 19.csv",skip=3)
 aux3 <-aux3 %>%  select(entidad, contacto_tot, contacto)
 
 
 #Total trámites en los que existió experiencia de corrupción
-aux4 <- read_csv("Variables/Corrupción/Utilizadas/otal trámites en los que existió experiencia de corrupción 19.csv", skip =1)
+aux4 <- read_csv("Variables/Corrupción/Modificadas manualmente/otal trámites en los que existió experiencia de corrupción 19.csv", skip =1)
 
 
 bd19 <- aux1 %>%  inner_join(aux2, by = "entidad") %>%  inner_join(aux3, by = "entidad") %>% 
@@ -96,3 +111,9 @@ bd19 <- aux1 %>%  inner_join(aux2, by = "entidad") %>%  inner_join(aux3, by = "e
 
 
 corrupcion <-bind_rows(bd15, bd17, bd19)
+corrupcion <- corrupcion %>%  select(-contains(c("tot"))) %>% 
+  rename(pob_18 = `Población de 18 años y más`, pob_usuario  = `Población de 18 años y más que tuvo contacto con algún servidor público`) %>% 
+  filter(entidad != "Estados Unidos Mexicanos")
+
+corrupcion %>%  write_excel_csv("Variables/Corrupción/Finales/corrupcion.csv")
+
---
+++ R/modelo.R
@@ -0,0 +1,82 @@
+library(dplyr)
+library(tidyverse)
+library(Hmisc)
+library(ggpubr)
+library(purrr)  
+library(ggplot2)
+library(broom)
+
+
+# Bases -------------------------------------------------------------------
+corrupcion <- read_csv("Variables/Corrupción/Finales/corrupcion.csv")
+pib <- read_csv("Variables/Corrupción/Finales/pib.csv")
+control <- read_csv("Variables/Corrupción/Finales/control.csv")
+
+bd <- corrupcion %>%  left_join(pib, by = c("entidad", "year")) %>% 
+  mutate(corrupcion = Muy_frecuente+Frecuente,
+         camara = Diputados_y_Senadores_Muy_frecuente+Diputados_y_Senadores_Frecuente,
+         ejercito =Ejército_y_Marina_Muy_frecuente+Ejército_y_Marina_Frecuente,
+         federal = Gobierno_Federal_Muy_frecuente+Gobierno_Federal_Frecuente,
+         estatal = Gobiernos_Estatales_Muy_frecuente+Gobiernos_Estatales_Frecuente,
+         municipal = Gobiernos_Municipales_Muy_frecuente+ Gobiernos_Municipales_Frecuente,
+         ministerio = Ministerio_Publico_Muy_frecuente+Ministerio_Publico_Frecuente,
+         policia = Policias_Muy_frecuente+Policias_Frecuente, 
+         contacto = contacto/pob_usuario, tramites =  tramites/pob_usuario) %>%  
+  select(entidad,year, corrupcion, camara,federal, estatal, municipal,
+         ejercito, ministerio, policia, contacto, tramites, 
+         starts_with(match = "pib_"))
+
+
+# r2 ----------------------------------------------------------------------
+
+bd %>% select(- starts_with(c("pib_")), -entidad) %>% 
+  map(~lm(bd$pib_total~ .x, data = bd)) %>% 
+  map(summary) %>% 
+  map_dbl("r.squared") %>% 
+  tidy %>% 
+  dplyr::arrange(desc(x)) %>% 
+  rename(r.squared = x) 
+
+bd %>% select(- starts_with(c("pib_")), -entidad, -year) %>% 
+  map(~lm(bd$pib_terciarias~ .x, data = bd)) %>% 
+  map(summary) %>% 
+  map_dbl("r.squared") %>% 
+  tidy %>% 
+  dplyr::arrange(desc(x)) %>% 
+  rename(r.squared = x) 
+
+bd %>% select(- starts_with(c("pib_")), -entidad, -year) %>% 
+  map(~lm(bd$pib_secundarias~ .x, data = bd)) %>% 
+  map(summary) %>% 
+  map_dbl("r.squared") %>% 
+  tidy %>% 
+  dplyr::arrange(desc(x)) %>% 
+  rename(r.squared = x) 
+
+bd %>% select(- starts_with(c("pib_")), -entidad, -year) %>% 
+  map(~lm(bd$pib_primarias~ .x, data = bd)) %>% 
+  map(summary) %>% 
+  map_dbl("r.squared") %>% 
+  tidy %>% 
+  dplyr::arrange(desc(x)) %>% 
+  rename(r.squared = x) 
+
+bd %>% select(- starts_with(c("pib_")), -entidad, -year) %>% 
+  map(~lm(bd$pib_educacion~ .x, data = bd)) %>% 
+  map(summary) %>% 
+  map_dbl("r.squared") %>% 
+  tidy %>% 
+  dplyr::arrange(desc(x)) %>% 
+  rename(r.squared = x)
+
+bd %>% select(- starts_with(c("pib_")), -entidad, -year) %>% 
+  map(~lm(bd$pib_salud~ .x, data = bd)) %>% 
+  map(summary) %>% 
+  map_dbl("r.squared") %>% 
+  tidy %>% 
+  dplyr::arrange(desc(x)) %>% 
+  rename(r.squared = x)
+
+bd %>%  select(pib_secundarias, contacto) %>%  na.omit() %>% 
+  cor
+
